d6cea4b9eb7a14f29903ea7c9cdd42b1e078aaa4
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-22 21:37:18 -0500

    Message Scalffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "homepage#index"
-  
+  resources :messages
+  root 'homepage#index'
   get 'homepage/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
